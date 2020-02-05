78dfa72620484bf2869d6c3937743b3504346bae
PashaFast <pashahit@gmail.com>
2020-02-05 19:02:49 +0300

    feat: Add task Filters. Advanced.ToBeContinue

---
+++ NET.Winter.2020.Staselko.08/Filters.Advanced.ToBeContinue/ArrayExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Filters.Advanced.ToBeContinue
+{
+    public static partial class ArrayExtension
+    {
+        public static int[] FilterArrayByKey(int[] source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source), "cannot be null!");
+            }
+
+            if (source.Length == 0)
+            {
+                throw new ArgumentException("cannot be empty!", nameof(source));
+            }
+
+            List<int> result = new List<int>();
+
+            foreach (var t in source)
+            {
+                AddToArrayIfMatch(result, t);
+            }
+
+            return result.ToArray();
+        }
+
+        static partial void AddToArrayIfMatch(List<int> array, int number);
+    }
+}
+
